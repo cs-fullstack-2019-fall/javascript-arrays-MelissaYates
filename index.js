0485bd46ba9c38e8df8c57a96dcd2fa1bb98dc92
Melissa Yates <melissampeart@gmail.com>
2019-08-13 12:40:48 -0500

    class work assignment arrays

---
+++ index.js
@@ -0,0 +1,35 @@
+// // Exercise 1
+// // Create an array.
+// //     Add four names to the array.
+// //     Print the array
+//
+// let names =[];
+// names.push("Melissa");
+// names.push("Sabrina");
+// names.push("Latoya");
+// names.push("Aaron");
+// for(var i = 0; i<names.length; i++){
+//     alert(names[i]);
+// }
+
+
+// Exercise 2
+// Create an array with 5 numbers in it.
+//     Print the numbers.
+
+// let numbersInt = [1,24,56,78,567];
+// for(let index in numbersInt){
+//     alert(numbersInt[index]);
+// }
+
+
+//     Exercise 3
+// Create an array with 4 names.
+//     Print the third item in the array.
+//     Delete the second item.
+//     Print the third item in the array again.
+
+let names = ["Melissa", "Latrice","Tiffany", "Nigel"];
+for(let i in names){
+    console.log(names[2]);
+}
